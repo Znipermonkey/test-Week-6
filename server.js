856d400b692c293acf1e6bbb74d4fb1122c29410
Robert Free <144733501+Znipermonkey@users.noreply.github.com>
2023-10-20 15:42:13 -0600

    finished rollbar

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require("express");
 const bots = require("./src/botsData");
 const shuffle = require("./src/shuffle");
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'fb3271539bb1403ba8058654e2903bae',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 const playerRecord = {
   wins: 0,
   losses: 0,
@@ -40,9 +49,11 @@ const calculateHealthAfterAttack = ({ playerDuo, compDuo }) => {
 app.get("/api/robots", (req, res) => {
   try {
     res.status(200).send(botsArr);
+    rollbar.info('sucessfully sent bots array')
   } catch (error) {
     console.error("ERROR GETTING BOTS", error);
     res.sendStatus(400);
+    rollbar.error('did not send bots array')
   }
 });
 
@@ -50,9 +61,11 @@ app.get("/api/robots/shuffled", (req, res) => {
   try {
     let shuffled = shuffle(bots);
     res.status(200).send(shuffled);
+    rollbar.info('sucessfully shuffled bots')
   } catch (error) {
     console.error("ERROR GETTING SHUFFLED BOTS", error);
     res.sendStatus(400);
+    rollbar.error('did not properly shuffle bots')
   }
 });
 
